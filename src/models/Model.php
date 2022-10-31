0bff785ad3488fd6873a868b1eb88b817ab374d5
unknown <andrelluis17@hotmail.com>
2022-10-31 14:01:40 -0300

    gerando comando Select , obtendo dados do banco e transformando em objetos

--- src/models/Model.php
+++ src/models/Model.php
@@ -24,4 +24,55 @@ class Model {
    public function __set($key, $value) {
         $this->values[$key] = $value;
    }
+
+
+   public static function get($filters = [], $columns = '*') {
+     $objects = [];
+     $result = static::getResultSetFromSelect($filters, $columns);
+     if($result) {
+          $class = get_called_class();
+          while($row = $result->fetch_assoc()) {
+               array_push($objects , new $class($row));
+          }
+     }
+     return $objects;
+   }
+
+   public static function getResultSetFromSelect($filters = [], $columns = '*') {
+     $filtersFormated = '';
+     if(count($filters) > 0) {
+         $filtersFormated = ' WHERE ' . static::getFilters($filters);
+     }
+
+     $sql = 'SELECT ' . $columns .  ' FROM ' . static::$tableName . $filtersFormated . ';';
+     $result = Database::getResultFromQuery($sql);
+     if($result->num_rows === 0) {
+          return null;
+     } else {
+          return $result;
+     }
+   }
+
+   private static function getFilters($filters) {
+     $sql = ' ';
+     $firstRound = true;
+     foreach($filters as $key => $value) {
+          if(!$firstRound){
+               $sql .= " AND";
+          }
+          $firstRound = false;
+          $sql .= " {$key} = " . static::getFormatedValue($value) . " " ;
+     }
+     return $sql;
+   }
+
+   private static function getFormatedValue($value) {
+     if(is_null($value)) {
+          return "null";
+     } else if(is_string($value)) {
+          return "'$value'";
+     } else {
+          return $value;
+     }
+   }
 }
