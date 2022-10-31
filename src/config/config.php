5fd9676e371b6b6ec42bab59e387c83b9f5df862
unknown <andrelluis17@hotmail.com>
2022-10-31 18:47:35 -0300

    Tela de autenticacao

--- src/config/config.php
+++ src/config/config.php
@@ -6,5 +6,6 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8' , 'portuguese');
 // Pastas;
 
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
 
 require_once(realpath(dirname(__FILE__) . '/database.php'));
